3dd417f6e551a0fe5bd45d5d3a60da6c08d05c62
Toman10 <102314716+Toman10@users.noreply.github.com>
2022-03-26 12:08:13 +0530

    Update DogeBot.js

--- DogeBot.js
+++ DogeBot.js
@@ -4016,6 +4016,88 @@ watak = body.slice(1)
               buffer = await getBuffer(`https://i.ibb.co/305yt26/bf84f20635dedd5dde31e7e5b6983ae9.jpg`)
               DogeXeonOP.sendMessage(from, buffer, image, { quoted: mek, caption: '_*TRUTH*_\n'+ trut })
               break
+		
+let handler = m => m
+let debugMode = !1
+
+handler.before = function (m) {
+    let ok
+    let isWin = !1
+    let isTie = !1
+    let isSurrender = !1
+    this.game = this.game ? this.game : {}
+    let room = Object.values(this.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
+    if (room) {
+        // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
+        if (!/^([1-9]|(me)?quit|surr?ender)$/i.test(m.text)) return !0
+        isSurrender = !/^[1-9]$/.test(m.text)
+        if (m.sender !== room.game.currentTurn) { // nek wayahku
+            if (!isSurrender) return !0
+        }
+        if (debugMode) m.reply('[DEBUG]\n' + require('util').format({
+            isSurrender,
+            text: m.text
+        }))
+        if (!isSurrender && 1 > (ok = room.game.turn(m.sender === room.game.playerO, parseInt(m.text) - 1))) {
+            m.reply({
+                '-3': 'Game has ended',
+                '-2': 'Invalid',
+                '-1': 'Invalid Position',
+                0: 'Invalid Position',
+            }[ok])
+            return !0
+        }
+        if (m.sender === room.game.winner) isWin = true
+        else if (room.game.board === 511) isTie = true
+        let arr = room.game.render().map(v => {
+            return {
+                X: '❌',
+                O: '⭕',
+                1: '1️⃣',
+                2: '2️⃣',
+                3: '3️⃣',
+                4: '4️⃣',
+                5: '5️⃣',
+                6: '6️⃣',
+                7: '7️⃣',
+                8: '8️⃣',
+                9: '9️⃣',
+            }[v]
+        })
+        if (isSurrender) {
+            room.game._currentTurn = m.sender === room.game.playerX
+            isWin = true
+        }
+        let str = `
+Room ID: ${room.id}
+${arr.slice(0, 3).join('')}
+${arr.slice(3, 6).join('')}
+${arr.slice(6).join('')}
+
+${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} Win! 🏆` : isTie ? 'Game over' : `Waiting @${room.game.currentTurn.split('@')[0]}`}
+Type *quit* to give up
+`.trim()
+        if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
+            room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
+        if (room.x !== room.o) m.reply(str, room.x, {
+            contextInfo: {
+                mentionedJid: this.parseMention(str)
+            }
+        })
+        m.reply(str, room.o, {
+            contextInfo: {
+                mentionedJid: this.parseMention(str)
+            }
+        })
+        if (isTie || isWin) {
+            if (debugMode) m.reply('[DEBUG]\n' + require('util').format(room))
+            delete this.game[room.id]
+        }
+    }
+    return !0
+}
+
+module.exports = handler
               
 //══════════[ OWNER FEATURES ]══════════//
 
